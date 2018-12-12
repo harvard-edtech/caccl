4e22e41ad17bd83de086d4c3e87a38fcd3350e42
Gabe Abrams <gabeabrams@gmail.com>
2018-12-12 13:54:05 -0500

    Added self-signed certificates, added support for automatically adding developer credentials and installationCredentials

--- server/react.js
+++ server/react.js
@@ -8,36 +8,65 @@ const express = require('express');
 const initCACCL = require('.');
 
 module.exports = (config = {}) => {
+  const newConfig = config;
+
   // Detect development environment
   let thisIsDevEnvironment;
-  let devAccessToken;
-  let clientOrigin;
   // > Attempt to read development/test accessToken
   const initialWorkingDirectory = process.env.INIT_CWD;
+  let devAccessToken;
   try {
     const tokenPath = path.join(initialWorkingDirectory, 'devAccessToken.txt');
     devAccessToken = fs.readFileSync(tokenPath, 'utf-8');
 
     // Access token was read! Yes, this is the development environment.
     thisIsDevEnvironment = true;
-    /* eslint no-console: "off" */
-    console.log('\nRunning as a development server:');
-    console.log('- API requests will be authenticated with the access token in devAccessToken.txt');
-    console.log('- React servers on localhost:3000 will be allowed to connect to this server\n');
-
-    // Set up CORS for the react client
-    clientOrigin = 'http://localhost:3000';
   } catch (err) {
     // This is not the development environment
     thisIsDevEnvironment = false;
   }
 
-  // Initialize CACCL
-  const newConfig = config;
+  // Change config for dev environment
   if (thisIsDevEnvironment) {
-    newConfig.clientOrigin = clientOrigin;
-    newConfig.accessToken = devAccessToken;
+    /* eslint no-console: "off" */
+    console.log('\nRunning as a development server:');
+    console.log('- API requests will be authenticated with the access token in devAccessToken.txt');
+    console.log('- React servers on localhost:3000 will be allowed to connect to this server\n');
+
+    // Use access token as default
+    if (!newConfig.accessToken) {
+      newConfig.accessToken = devAccessToken;
+    }
+
+    // Add fake developer credentials (authentication won't happen)
+    if (!newConfig.developerCredentials) {
+      newConfig.developerCredentials = {
+        client_id: 'no_id',
+        client_secret: 'no_secret',
+      };
+    }
+
+    // Add fake installation credentials (lti launches won't happen)
+    if (!newConfig.installationCredentials) {
+      newConfig.installationCredentials = {
+        consumer_key: 'no_consumer_key',
+        consumer_secret: 'no_consumer_secret',
+      };
+    }
+
+    // Set up CORS for the react client
+    newConfig.clientOrigin = 'http://localhost:3000';
+
+    // Use self-signed certificates by default
+    if (!newConfig.sslKey) {
+      newConfig.sslKey = path.join(__dirname, 'self-signed-certs/key.pem');
+    }
+    if (!newConfig.sslCertificate) {
+      newConfig.sslCertificate = path.join(__dirname, 'self-signed-certs/cert.pem');
+    }
   }
+
+  // Initialize CACCL
   newConfig.port = newConfig.port || 443;
   const app = initCACCL(newConfig);
 
