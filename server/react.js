a8f964f3bcf39b041e3ce5059b87329a3953a3a0
Gabe Abrams <gabeabrams@gmail.com>
2018-12-12 11:57:13 -0500

    Added built-in support for react-based servers

---
+++ server/react.js
@@ -0,0 +1,53 @@
+// A special setup of the server that corresponds to a react-environment
+// with the react app in the client/ folder
+
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const initCACCL = require('.');
+
+module.exports = (config = {}) => {
+  // Detect development environment
+  let thisIsDevEnvironment;
+  let devAccessToken;
+  let clientOrigin;
+  // > Attempt to read development/test accessToken
+  const initialWorkingDirectory = process.env.INIT_CWD;
+  try {
+    const tokenPath = path.join(initialWorkingDirectory, 'devAccessToken.txt');
+    devAccessToken = fs.readFileSync(tokenPath, 'utf-8');
+
+    // Access token was read! Yes, this is the development environment.
+    thisIsDevEnvironment = true;
+    /* eslint no-console: "off" */
+    console.log('\nRunning as a development server:');
+    console.log('- All API requests will be authenticated using the access token in devAccessToken.txt');
+    console.log('- Any react server running on localhost:3000 will be allowed to connect to this server\n');
+
+    // Set up CORS for the react client
+    clientOrigin = 'http://localhost:3000';
+  } catch (err) {
+    // This is not the development environment
+    thisIsDevEnvironment = false;
+  }
+
+  // Initialize CACCL
+  const newConfig = config;
+  if (thisIsDevEnvironment) {
+    newConfig.clientOrigin = clientOrigin;
+    newConfig.accessToken = devAccessToken;
+  }
+  const app = initCACCL(newConfig);
+
+  // If production, serve built client app
+  if (!thisIsDevEnvironment) {
+    // Serve styles, etc
+    app.use(express.static(path.join(initialWorkingDirectory, 'client', 'build')));
+
+    // Send frontend
+    app.get('/', (req, res) => {
+      res.sendFile(path.join(initialWorkingDirectory, 'client', 'build', 'index.html'));
+    });
+  }
+};
