c1c835aeae2cc484fc66072b144c1f488930878a
Gabe Abrams <gabeabrams@gmail.com>
2018-12-12 10:45:41 -0500

    Added easily accessible reference to api class

---
+++ APIInstanceClass.js
@@ -0,0 +1,4 @@
+// A simple reference to the API instance class for type checking
+const APIInstanceClass = require('../caccl-api/classes/EndpointCategory'); // TODO: use real module
+
+module.exports = APIInstanceClass;
